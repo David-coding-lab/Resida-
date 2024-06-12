1a12ea67a2ee7ddc24ddee8adb27a5172c1d5949
Emeka David <emekad745@gmail.com>
2024-06-12 13:31:11 +0100

    fixed responsive issues

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(
-    <di>
+    <div>
         <App />
-    </di>
+    </div>
 )
